fa72bac291ca6eb28e239e3a89b1624dbe65c4b4
am2064 <am2064@nova.edu>
2014-03-01 16:57:17 -0500

    asdf

--- makethis.sh
+++
@@ -1,4 +0,0 @@
-php artisan generate:resource bonus --fields="name:string,owner_id:integer,bonus:integer,stat:string"
-php artisan generate:resource weapon --fields="name:string,owner_id:integer,damage:string,bonus:integer,description:text,effect:text"
-php artisan generate:resource armor --fields="name:string,owner_id:integer,defense:string,description:text,effect:text"
-php artisan generate:resource spell --fields="name:string,owner_id:integer,damage:string,bonus:integer,description:text,effect:text"
